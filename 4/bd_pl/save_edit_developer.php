1bb2ed18483c3f6948370efcae810a113e681915
fadeevdan77 <97964899+ilyasovmaksim22@users.noreply.github.com>
2022-01-18 20:27:49 +0500

    asd

--- 4/bd_pl/save_edit_developer.php
+++ 4/bd_pl/save_edit_developer.php
@@ -2,9 +2,9 @@
 
 <html><body>
 <?php
- $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b844245c408b92","a1683317", "heroku_1f01e7efa26acd8") or die ("Невозможно подключиться к серверу");
+ $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b7cf324568026c","90ce335c", "heroku_666d3fc091d73be") or die ("Невозможно подключиться к серверу");
  mysqli_query($conn, 'SET NAMES cp1251');
- $zapros="UPDATE alien SET name='".$_GET['name']. "', info='".$_GET['info']."' WHERE id=".$_GET['id'];
+ $zapros="UPDATE developer SET name='".$_GET['name']. "', city='".$_GET['city']."' WHERE id=".$_GET['id'];
  mysqli_query($conn, $zapros);
 if (mysqli_affected_rows($conn)>0) {
  echo 'Все сохранено. <a href="index.php"> Вернуться </a>'; }
