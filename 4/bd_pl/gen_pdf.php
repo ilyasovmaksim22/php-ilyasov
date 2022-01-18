48fa2992143e12cadabfd5420da3bd0757add751
fadeevdan77 <97964899+ilyasovmaksim22@users.noreply.github.com>
2022-01-18 20:51:38 +0500

    asd

--- 4/bd_pl/gen_pdf.php
+++ 4/bd_pl/gen_pdf.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b844245c408b92","a1683317", "heroku_1f01e7efa26acd8") or die ("Невозможно подключиться к серверу");
+  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b7cf324568026c","90ce335c", "heroku_666d3fc091d73be") or die ("Невозможно подключиться к серверу");
   mysqli_query($conn, "SET NAMES cp1251");
 
   define('FPDF_FONTPATH',"../../fpdf/font/");
@@ -17,45 +17,43 @@
   $pdf -> SetFont("Arial", "", "6");
 
   $pdf -> Cell(5, 5, "№", 1, 0, "C");
-  $pdf -> Cell(25, 5, "Планета", 1, 0, "C");
-  $pdf -> Cell(25, 5, "Созвездие", 1, 0, "C");
-  $pdf -> Cell(25, 5, "Расстояние, млн. км.", 1, 0, "C");
-  $pdf -> Cell(20, 5, "Тип", 1, 0, "C");
-  $pdf -> Cell(25, 5, "Диаметр, км.", 1, 0, "C");
-  $pdf -> Cell(25, 5, "Вид инопланетян", 1, 0, "C");
-  $pdf -> Cell(25, 5, "Количество, тыс.", 1, 1, "C");
+  $pdf -> Cell(25, 5, "Название приложения", 1, 0, "C");
+  $pdf -> Cell(25, 5, "Текущая версия", 1, 0, "C");
+  $pdf -> Cell(25, 5, "Название разработчика", 1, 0, "C");
+  $pdf -> Cell(20, 5, "Город", 1, 0, "C");
+  $pdf -> Cell(25, 5, "Тип выполнения", 1, 0, "C");
+  $pdf -> Cell(25, 5, "Тип (комп. / интер.)", 1, 1, "C");
 
   $pdf -> SetFont("Arial", "", "5");
 
-  $query = mysqli_query($conn, "SELECT * FROM population");
-  for($i = 1; $fetch_request = mysqli_fetch_array($query); $i++) {
-    $id_planet = $fetch_request["id_planet"];
-    $id_alien = $fetch_request["id_alien"];
-    $count = $fetch_request["count"];
+  $query_app = mysqli_query($conn, "SELECT * FROM app");
+  for($i = 1; $fetch_app = mysqli_fetch_array($query_app); $i++) {
+    $name_app = $fetch_app["name"];
+    $ver = $fetch_app["ver"];
+    $id_pl = $fetch_app["id_pl"];
+    $id_developer = $fetch_app["id_developer"];
 
-    $query_fridge = mysqli_query($conn, "SELECT * FROM planet WHERE id = '" . $id_planet . "'");
-    if($fetch_fridge = mysqli_fetch_array($query_fridge)) {
-      $name_planet = $fetch_fridge["name"];
-      $galaxy = $fetch_fridge["galaxy"];
-      $distance = $fetch_fridge["distance"];
-      $type = $fetch_fridge["type"];
-      $diam = $fetch_fridge["diam"];
+    $query_developer = mysqli_query($conn, "SELECT * FROM developer WHERE id = '" . $id_developer . "'");
+    if($fetch_developer = mysqli_fetch_array($query_developer)) {
+      $name_developer = $fetch_developer["name"];
+      $city = $fetch_developer["city"];
     }
    
-    $query_service = mysqli_query($conn, "SELECT * FROM alien WHERE id = '" . $id_alien . "'");
-    if($fetch_service = mysqli_fetch_array($query_service)) {
-      $name_alien = $fetch_service["name"];
+    $query_pl = mysqli_query($conn, "SELECT * FROM pl WHERE id = '" . $id_pl . "'");
+    if($fetch_pl = mysqli_fetch_array($query_pl)) {
+      $name_pl = $fetch_pl["name"];
+      $exec = $fetch_pl["exec"];
+      $type = $fetch_pl["type"];
     }
 
     $pdf -> Cell(5, 5, $i, 1, 0, "C");
-    $pdf -> Cell(25, 5, $name_planet, 1, 0, "C");
-    $pdf -> Cell(25, 5, $galaxy, 1, 0, "C");
-    $pdf -> Cell(25, 5, $distance, 1, 0, "C");
-    $pdf -> Cell(20, 5, $type, 1, 0, "C");
-    $pdf -> Cell(25, 5, $diam, 1, 0, "C");
-    $pdf -> Cell(25, 5, $name_alien, 1, 0, "C");
-    $pdf -> Cell(25, 5, $count, 1, 1, "C");
+    $pdf -> Cell(25, 5, $name_app, 1, 0, "C");
+    $pdf -> Cell(25, 5, $ver, 1, 0, "C");
+    $pdf -> Cell(25, 5, $name_developer, 1, 0, "C");
+    $pdf -> Cell(20, 5, $city, 1, 0, "C");
+    $pdf -> Cell(25, 5, $exec, 1, 0, "C");
+    $pdf -> Cell(25, 5, $type, 1, 1, "C");
 }
 
-$pdf -> Output("katasonov_12.pdf", "D");
+$pdf -> Output("ilyasov_11.pdf", "D");
 ?>
