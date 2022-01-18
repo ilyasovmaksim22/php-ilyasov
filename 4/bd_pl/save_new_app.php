d4e10db2e1ad805fa03756cafe4551aac838c674
fadeevdan77 <97964899+ilyasovmaksim22@users.noreply.github.com>
2022-01-18 20:22:22 +0500

    asd

--- 4/bd_pl/save_new_app.php
+++ 4/bd_pl/save_new_app.php
@@ -5,7 +5,7 @@
  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b7cf324568026c","90ce335c", "heroku_666d3fc091d73be") or die ("Невозможно подключиться к серверу");
  mysqli_query($conn, 'SET NAMES cp1251'); // Тип кодировки
  // Строка запроса на добавление записи в таблицу:
- $sql_add =  $sql_add = "INSERT INTO app SET id_lp='" . $_GET['id_lp']
+ $sql_add =  $sql_add = "INSERT INTO app SET id_pl='" . $_GET['id_pl']
 ."', id_developer='".$_GET['id_developer']."', date='".$_GET['date']."', ver='".$_GET['ver']."', name='".$_GET['name']."'";
  mysqli_query($conn, $sql_add); // Выполнение запроса
  if (mysqli_affected_rows($conn)>0) // если нет ошибок при выполнении запроса
