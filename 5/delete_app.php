6168094b9071fec97b296d382c2d88fcab143f27
fadeevdan77 <97964899+ilyasovmaksim22@users.noreply.github.com>
2022-01-18 21:20:09 +0500

    asd

--- 5/delete_app.php
+++ 5/delete_app.php
@@ -1,7 +1,10 @@
 <?php
+session_start();
+if($_SESSION["rule"] == 2) {
  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b7cf324568026c","90ce335c", "heroku_666d3fc091d73be") or die ("Невозможно подключиться к серверу");
  $zapros="DELETE FROM app WHERE id=" . $_GET['id'];
  mysqli_query($conn, $zapros);
+}
  header("Location: index.php");
  exit;
 ?>
